6c1207f8f202947dc042774e06f08aeb8767b387
John <ife.muyiwa54@gmail.com>
2021-01-22 19:20:22 +0100

    Initial Commit

--- GraphQLSampleAppUI/Program.cs
+++ GraphQLSampleAppUI/Program.cs
@@ -1,11 +1,7 @@
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using System;
-using System.Collections.Generic;
 using System.Net.Http;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace GraphQLSampleAppUI
@@ -17,8 +13,8 @@ namespace GraphQLSampleAppUI
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
             builder.RootComponents.Add<App>("#app");
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
-
+            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });                    
+           
             await builder.Build().RunAsync();            
         }
     }
